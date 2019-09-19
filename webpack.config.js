8d775296749306a45f30cdfae1c237b9c23482fe
Adrian Garcia <garciasaaib@gmail.com>
2019-09-19 16:32:48 -0600

    wip: dev enviroment done, with css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
@@ -22,5 +25,10 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 }
